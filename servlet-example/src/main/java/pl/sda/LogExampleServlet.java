0e3aea02e2082dd15528a50b3fdf6c2d63e6ab31
Marcin <m.wygledowski92@gmail.com>
2019-07-07 12:16:18 +0200

    Zmiana nazwy pakietu z sda na pl.sda

--- servlet-example/src/main/java/pl/sda/LogExampleServlet.java
+++ servlet-example/src/main/java/pl/sda/LogExampleServlet.java
@@ -1,4 +1,4 @@
-package sda;
+package pl.sda;
 
 import org.apache.log4j.Logger;
 
--- servlet-example/src/main/java/pl/sda/MyFirstServlet.java
+++ servlet-example/src/main/java/pl/sda/MyFirstServlet.java
@@ -1,4 +1,4 @@
-package sda;
+package pl.sda;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
